3862a5405d0aab73cb3596d8be3f12dc3c0408f6
Jegath S <jegath@intelops.dev>
2023-10-26 10:17:55 +0530

    updated package version to 0.0.4

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
     name='scsctl',
     description= 'Tool for automating Vulnerability Risk Management and Software Supply Chain Security Measures',
-    version='0.0.1',
+    version='0.0.4',
     py_modules=['scsctl'],
-    install_requires=['click==8.1.3', 'clickhouse-driver==0.2.6', 'numpy==1.25.0', 'requests==2.31.0','questionary==1.10.0','tabulate==0.9.0'],
+    install_requires=['click==8.1.3', 'clickhouse-driver==0.2.6', 'numpy==1.25.0', 'requests==2.31.0','questionary==1.10.0','tabulate==0.9.0','hvac==1.2.1','fastapi==0.103.1','uvicorn==0.23.2','kubernetes==27.2.0'],
     entry_points={
         'console_scripts': [
             'scsctl = scsctl.app:cli'
